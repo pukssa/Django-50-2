068f2c286411790019cd825d83b62ac74fadd156
pukssa <argen@MacBook-Air--puksa.local>
2025-03-28 22:04:52 +0600

    HW-6

--- posts/forms.py
+++ posts/forms.py
@@ -1,7 +1,32 @@
 from .models import Post, Category, Tag
 from django import forms
+from posts.models import Category
 
 class PostCreateForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'content', 'category', 'tags', 'image', 'rate')
+
+class SearchForm(forms.Form):
+    search = forms.CharField
+    required = False
+    max_length = 100
+    widget = forms.TextInput(attrs={'placeholder': 'Search'})
+
+
+    category = forms.ModelChoiceField(queryset=Category.objects.all(), required=False )
+    tags =  forms.ModelMultipleChoiceField(queryset=Tag.objects.all())
+
+orderings = (
+    'title',
+    "-title",
+    'rate'
+    '-rate',
+    'created',
+    '-created',
+)
+
+ordering = (forms.ChoiceField
+            (choices=orderings, widget = forms.Select(attrs={
+    'class': 'form-control'
+})))
--- posts/views.py
+++ posts/views.py
@@ -1,26 +1,81 @@
+from unicodedata import category
+
 from django.shortcuts import render, HttpResponse
 import random
-from .forms import PostCreateForm
+from .forms import PostCreateForm, SearchForm
 import posts
 from posts.models import Post
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 
 
 def test_view(request):
     return HttpResponse(f"twinkling watermelon {random.randint(1, 100)}")
 
+
+
 def html_view(request):
     return render(request,"main.html")
+
+
+
+
 @login_required(login_url="/login/")
 def post_list_view(request):
-    post = (Post.objects.all())
-    print(posts)
-    return render(request,"posts/post_list.html",{"posts":post})
+    form = SearchForm()
+    query_params = request.GET
+    limit = 3
+    if request.method == "GET":
+
+        posts = Post.objects.all()
+        category_id = query_params.get("category")
+        tags = query_params.get("tags")
+        search = query_params.get("search")
+        ordering = query_params.get("ordering")
+        page = int(query_params.get("page",)) if query_params.get("page") else 1
+
+        if search:
+             posts = posts.filter(Q(title__icontains=search) | Q(content__icontains=search)
+            )
+
+        if category_id:
+            posts = posts.filter(category=category_id)
+
+        if tags:
+            tags = [int(tag) for tag in tags]
+            posts = posts.filter(tag__in=tags)
+
+
+        if ordering:
+            posts = posts.order_by(ordering)
+
+
+        max_pages = posts.count() // limit
+        if round(max_pages) < limit:
+            max_pages = round(max_pages) + 1
+        else:
+            max_pages = round (max_pages)
+
+        start = (page -1) * limit
+        end = page * limit
+        posts = posts[start:end]
+        context_data = (
+        "posts":posts,
+        'form': form,
+        "max_pages": range(1, max_pages + 1) )
+        return render(request, "post_list.html",context=context_data
+        )
+
+
 @login_required(login_url="/login/")
 def post_detail_view(request, post_id):
     post = Post.objects.get(id=post_id)
     return render(request,"posts/post_detail.html", {"posts":post})
+
+
+
+
 @login_required(login_url="/login/")
 def create_post_view(request):
     if request.method == "GET":
